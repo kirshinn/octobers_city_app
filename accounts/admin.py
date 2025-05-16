17cc0fb4737cc290f8e80bc4c7b7a37f5bb8dbad
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2025-05-16 20:55:04 +0700

    update app accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser  # расширенная модель User
 
-# Register your models here.
+admin.site.register(CustomUser, UserAdmin)
