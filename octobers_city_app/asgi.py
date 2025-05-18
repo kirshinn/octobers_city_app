a550e1a8fde990c23412b5fe051b5c1c6d55a778
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2025-05-18 19:53:19 +0700

    rename in octobers_city_app

--- octobers_city_app/asgi.py
+++ octobers_city_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for baseproject project.
+ASGI config for octobers_city_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baseproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'octobers_city_app.settings')
 
 application = get_asgi_application()
--- octobers_city_app/wsgi.py
+++ octobers_city_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for baseproject project.
+WSGI config for octobers_city_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baseproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'octobers_city_app.settings')
 
 application = get_wsgi_application()
