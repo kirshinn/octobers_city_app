9989c8977c8c263f1575844dee58146419db0ff1
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2025-05-29 15:44:27 +0700

    add model status, modify model feedback

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
--- feedback/models.py
+++ feedback/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from accounts.models import CustomUser
 from django.utils import timezone
+from core.models import Status
+from accounts.models import CustomUser
 
 class Feedback(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
     subject = models.CharField(max_length=200)
     message = models.TextField()
+    answer = models.TextField(null=True, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, default=None, null=True, blank=True)
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
