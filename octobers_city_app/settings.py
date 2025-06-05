2b49572fa39797c9bded3a320a56e8fed17969e1
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2025-06-05 22:09:32 +0700

    partial translate~1

--- octobers_city_app/settings.py
+++ octobers_city_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4nqs(-5%%t1*72sahp=*6z9wkqoh_mv*wnl_e1m!r_ncjc=&%s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
